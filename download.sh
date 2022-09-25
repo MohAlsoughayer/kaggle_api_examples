7dba55feb66461ee111649dc6176422ada5a0d18
Mohammed Alsoughayer <mohs@bu.edu>
2022-09-25 14:05:13 +0300

    merge resolved

--- download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-kernels=$(kaggle kernels list --user jhoward | egrep -oh 'jhoward/(\S)*')
-
-while read n; do
-  echo "Downloading $n"
-  kaggle kernels pull $n -p "/Users/Moh/Documents/misk_skills/kaggle_api_examples/JHoward/"
-done <<< "$kernels"
-
